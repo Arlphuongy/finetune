d5e2709bc38e84ca6ae8f7b7ddcd74e63be07cf0
ThaiLe1220 <lehongthai2000@gmail.com>
2024-01-29 23:42:18 +0700

    update batch size

--- finetune.py
+++ finetune.py
@@ -54,7 +54,7 @@ tokenized_datasets = raw_datasets.map(preprocess_function, batched=True)
 
 model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_CHECKPOINT)
 
-BATCH_SIZE = 8
+BATCH_SIZE = 48
 MODEL_NAME = MODEL_CHECKPOINT.rsplit("/", maxsplit=1)[-1]
 
 args = Seq2SeqTrainingArguments(
