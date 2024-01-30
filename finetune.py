fb90eca4489362d51fc0b7ce1ca8cca85ac3c659
ThaiLe1220 <lehongthai2000@gmail.com>
2024-01-30 18:07:14 +0700

    es-en

--- finetune.py
+++ finetune.py
@@ -20,15 +20,15 @@ from transformers import (
 os.environ["WANDB_DISABLED"] = "true"
 
 # Set source and target languages for translation
-SOURCE_LANG = "en"
-TARGET_LANG = "es"
+SOURCE_LANG = "es"
+TARGET_LANG = "en"
 
 # Load dataset and metric for evaluation (BLEU score)
-raw_datasets = load_dataset("Eugenememe/netflix-en-es")
+raw_datasets = load_dataset("Eugenememe/netflix-es-en")
 metric = evaluate.load("sacrebleu")
 
 # Define tokenizer and model checkpoint from Hugging Face
-MODEL_CHECKPOINT = "Helsinki-NLP/opus-mt-en-es"
+MODEL_CHECKPOINT = "Helsinki-NLP/opus-mt-es-en"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
 
 # Set prefix, maximum input and output lengths for tokenization
@@ -65,7 +65,7 @@ args = Seq2SeqTrainingArguments(
     output_dir=f"{MODEL_NAME}-finetuned",
     evaluation_strategy="epoch",
     save_strategy="epoch",
-    learning_rate=2e-6,
+    learning_rate=1e-6,
     per_device_train_batch_size=BATCH_SIZE,
     per_device_eval_batch_size=BATCH_SIZE,
     weight_decay=0.01,
