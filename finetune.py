0e602002e9a9ea5bfc88d32491189454075eeeed
ThaiLe1220 <lehongthai2000@gmail.com>
2024-02-05 17:00:36 +0700

    ko-en

--- finetune.py
+++ finetune.py
@@ -20,15 +20,15 @@ from transformers import (
 os.environ["WANDB_DISABLED"] = "true"
 
 # Set source and target languages for translation
-SOURCE_LANG = "th"
+SOURCE_LANG = "ko"
 TARGET_LANG = "en"
 
 # Load dataset and metric for evaluation (BLEU score)
-raw_datasets = load_dataset("Eugenememe/netflix-th-en")
+raw_datasets = load_dataset("Eugenememe/netflix-ko-en")
 metric = evaluate.load("sacrebleu")
 
 # Define tokenizer and model checkpoint from Hugging Face
-MODEL_CHECKPOINT = "Helsinki-NLP/opus-mt-th-en"
+MODEL_CHECKPOINT = "Helsinki-NLP/opus-mt-ko-en"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
 
 # Set prefix, maximum input and output lengths for tokenization
