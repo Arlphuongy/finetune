0de09433f71129a9d23423dfec840be6ab3926d9
ThaiLe1220 <lehongthai2000@gmail.com>
2024-03-26 12:43:30 +0700

    hihi

--- performace_testing.py
+++ performace_testing.py
@@ -96,22 +96,20 @@ def translate_with_opus_mt(texts, model_name="Helsinki-NLP/opus-mt-en-vi"):
 
 if __name__ == "__main__":
     src_texts = [
-        # "George Soros Quotes. I'm only rich because I know when I'm wrong. It's not whether you're right or wrong, but how much money you make when you're right and how much you lose when you're wrong",
         "Van Thinh Phat chairwoman Truong My Lan has offered to hand in 13 family-owned assets as compensation for the money she is accused of appropriating from Saigon Commercial Bank.",
-        "Today, before the judges, I promise to submit my shares and the shares of my children and friends to the State Bank of Vietnam for the purpose of managing SCB, she told the People's Court of Ho Chi Minh City Tuesday.",
-        "Lawyers for Van Thinh Phat chairwoman Truong My Lan request a court review of her embezzlement and bribery charges after prosecutors recommended a death sentence based on the allegations.",
-        "The new victory for Indonesia sets the stage for a highly anticipated rematch, scheduled to take place in five days at Hanoi's My Dinh National Stadium. Both teams will be looking to refine their strategies and capitalize on this intense rivalry, with Vietnam aiming to avenge their narrow loss and Indonesia looking to maintain their winning momentum.",
-        "In a nail-biting World Cup qualifier, Indonesia secured a single-goal victory over Vietnam, thanks to a critical error from the guests' defense. The match saw both teams delivering intense performances, but it was Indonesia who capitalized on their chances.",
-        "Vietnam's benchmark VN-Index rose 1.30% to 1,276.42 points Thursday, highest since early September 2022.",
-        "Foreign investors were net sellers to the tune of VND365 billion, mainly selling VNM of dairy giant Vinamilk and MSN of conglomerate Masan Group.",
-        "The HNX-Index for stocks on the Hanoi Stock Exchange, home to mid and small caps, rose 1.31%, while the UPCoM-Index for the Unlisted Public Companies Market went up 0.30%.",
-        "Top congressional negotiators in the early hours of Thursday unveiled the $1.2 trillion spending bill to fund the government through September, though it remained unclear whether Congress would be able to complete action on it in time to avert a brief partial government shutdown over the weekend.",
-        "Lawmakers are racing to pass the legislation before a Friday midnight deadline in order to prevent a lapse in funds for over half the government, including the Department of Homeland Security, the Pentagon and health agencies. They are already six months behind schedule because of lengthy negotiations to resolve funding and policy disputes.",
-        "Democrats and Republicans both highlighted victories in the painstakingly negotiated legislation. Republicans cited as victories funding for Border Patrol agents, additional detention beds run by Immigration and Customs Enforcement, and a provision cutting off aid to the main United Nations agency that provides assistance to Palestinians. Democrats secured funding increases for federal child care and education programs, cancer and Alzheimer’s research.",
-        "The Justice Department joined 16 states and the District of Columbia to file an antitrust lawsuit against Apple on Thursday, the federal government’s most significant challenge to the reach and influence of the company that has put iPhones in the hands of more than a billion people.",
-        "The tech giant prevented other companies from offering applications that compete with Apple products like its digital wallet, which could diminish the value of the iPhone, the government said. Apple’s policies hurt consumers and smaller companies that compete with some of Apple’s services, in the form of “higher prices and less innovation,” the lawsuit said.",
-        "“Each step in Apple’s course of conduct built and reinforced the moat around its smartphone monopoly,” the government said in the lawsuit, which was filed in the U.S. District Court for the District of New Jersey.",
     ]
+
+    # Function to read lines from a file, check each line, and append to src_texts if conditions are met
+    def add_texts_from_file(file_path, target_list):
+        with open(file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()  # Remove leading/trailing whitespace
+                if (
+                    line and len(line) > 20
+                ):  # Check if line is not empty and has more than 20 characters
+                    target_list.append(line)
+
+    add_texts_from_file("content.txt", src_texts)
 
     # Redirect stdout to a file
     with open("model_performance.log", "w") as f:
